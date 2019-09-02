133b8647d88a9a150ef594b1a6183ebf683dac21
taylo <taylo@DESKTOP-7JDE6KA.fios-router.home>
2019-09-02 16:44:30 -0400

    First commit

---
+++ Quiz practice/src/Quiz.java
@@ -0,0 +1,21 @@
+
+public class Quiz 
+{
+	public static void main(String[]args)
+	{
+		/*System.out.println("\"\\\"\\t\"\\\\\"TJ");
+		System.out.println("TJ\\t\\t\"\\\t\\\"");
+		System.out.println("TJ\n\t\"Loves Hockey!\"");
+		*/
+		System.out.println(7 + " " + 8 + 9);
+		System.out.println(7 + " " + (8 + 9));
+		
+		System.out.println(7 + 8 + " " + 9);
+		System.out.println((7 + 8) + " " + 9);
+		
+		String tj = "TJ";
+		System.out.println(tj+"\n\t\"Loves Hockey!\"");
+
+	}
+
+}
