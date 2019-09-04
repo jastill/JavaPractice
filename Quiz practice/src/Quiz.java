ede233c049b4f730daef5bc342d99e747e402273
taylo <taylo@DESKTOP-7JDE6KA.fios-router.home>
2019-09-03 20:39:44 -0400

    Variables worksheet 1

--- Quiz practice/src/Quiz.java
+++ Quiz practice/src/Quiz.java
@@ -7,15 +7,79 @@ public class Quiz
 		System.out.println("TJ\\t\\t\"\\\t\\\"");
 		System.out.println("TJ\n\t\"Loves Hockey!\"");
 		*/
-		System.out.println(7 + " " + 8 + 9);
-		System.out.println(7 + " " + (8 + 9));
+		/*
+		 * System.out.println(7 + " " + 8 + 9); System.out.println(7 + " " + (8 + 9));
+		 * 
+		 * System.out.println(7 + 8 + " " + 9); System.out.println((7 + 8) + " " + 9);
+		 * 
+		 * String tj = "TJ"; System.out.println(tj+"\n\t\"Loves Hockey!\"");
+		 */
+		java.io.PrintStream out = System.out;		
 		
-		System.out.println(7 + 8 + " " + 9);
-		System.out.println((7 + 8) + " " + 9);
+		int x = 128;
+		 out.println(x);
+		 
+		 x = -98;
+		 out.println(x);
+		byte b = 24;
+		out.println(b);
 		
-		String tj = "TJ";
-		System.out.println(tj+"\n\t\"Loves Hockey!\"");
+		char c = 97;
+		out.println(c);
 
+		double d = 9.9;
+		out.println(d);
+		
+		d = 5.2;
+		out.println(d);
+		
+		float f = 9.87f;
+		out.println(f);
+		
+		short s = 350;
+		out.println(s);
+		
+		int z = 'A' + 5; 
+		out.println(z);
+		
+		c = 'A' + 5; 
+		out.println(c);
+		
+		double w = 'a' + 5;
+		out.println(w);
+		
+		long u = 'A' - 48;
+		out.println(u);
+		
+		w = f+5;
+		out.println(w);
+		
+		b = (byte)x;
+		out.println(b);
+		
+		w = 'A' + 2.0;
+		out.println(w);
+
+		/*
+		f = w;
+		out.println(w);
+		*/
+		
+		u = 982743l;
+		out.println(u);
+		
+		d = 3.2e2;
+		out.println(d);
+		
+		/*
+		 * s = c; out.println(u);
+		 */		
+		
+		/*
+		 * z = w; out.println(u);
+		 */
+
+		
 	}
 
 }
