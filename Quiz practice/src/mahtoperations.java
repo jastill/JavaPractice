d4fde842bab2525f38d78ef160310800e7d42564
taylo <taylo@DESKTOP-7JDE6KA.fios-router.home>
2019-09-10 19:56:54 -0400

    Order of operation practice

---
+++ Quiz practice/src/mahtoperations.java
@@ -0,0 +1,24 @@
+
+public class mahtoperations {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int	num = 27;
+		
+		System.out.println(num*=2);
+		System.out.println(num/=5);
+		System.out.println(num = num + 4 / 2 - 8);
+		System.out.println(num = (4 + 5)/2+7);
+		
+		System.out.println("1/2 = " + (1/2));
+		System.out.println("(double)1/2 = " + (double)1/2);
+		System.out.println("5/2 =  " + (5/2));
+		System.out.println("5/(double)2 =  " + 5/(double)2);
+		
+		
+		
+		
+
+	}
+
+}
