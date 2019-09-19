f4adaf802963a9c0c5879a848b500ff257500f54
taylo <taylo@DESKTOP-7JDE6KA.fios-router.home>
2019-09-18 22:26:56 -0400

    Added math work sheet

---
+++ Quiz practice/src/Math.java
@@ -0,0 +1,19 @@
+
+public class Math
+{
+	public  void one()
+	{
+		System.out.println("A+");
+	}
+
+	public void two()
+	{
+		System.out.println("B+");	
+	}
+	
+	public void three()
+	{
+		System.out.println("C+");
+		two();
+	}
+}
---
+++ Quiz practice/src/TestMath.java
@@ -0,0 +1,20 @@
+
+
+public class TestMath {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Math test = new Math();
+		test.one();
+		test.one();
+		test.two();
+		test.one();
+		test.two();
+		
+		test.one();
+		test.three();
+		test.two();
+	}
+
+}
